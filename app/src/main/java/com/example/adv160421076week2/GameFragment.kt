54407c463d804b4df7c9c42c59f3c5787b8163c9
DHSNIEL <sukidi.dh@gmail.com>
2024-02-26 12:05:40 +0700

    basic push

---
+++ app/src/main/java/com/example/adv160421076week2/GameFragment.kt
@@ -0,0 +1,41 @@
+package com.example.adv160421076week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.adv160421076week2.databinding.ActivityMainBinding
+import com.example.adv160421076week2.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(arguments != null) {
+            val playerName =
+                GameFragmentArgs.fromBundle(requireArguments()).playerName
+            binding.txtTurn.text = "$playerName's Turn"
+        }
+
+        val btnBack = view.findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/adv160421076week2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.adv160421076week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.adv160421076week2.databinding.ActivityMainBinding
+import com.example.adv160421076week2.databinding.FragmentGameBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        binding = ActivityMainBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
+
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.hostFragment) as
+                    NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
