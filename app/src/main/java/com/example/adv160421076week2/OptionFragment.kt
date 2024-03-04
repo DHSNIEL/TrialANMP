4948bc6dc0228ff57e2b0c7f5cad1b740f33bcce
DHSNIEL <sukidi.dh@gmail.com>
2024-03-04 11:51:57 +0700

    # Week 3 - Up to page 43 - fixed previous errors

--- app/src/main/java/com/example/adv160421076week2/OptionFragment.kt
+++ app/src/main/java/com/example/adv160421076week2/OptionFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.util.logging.Level
 
 class OptionFragment : BottomSheetDialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_option, container, false)
+    private val LEVEL = arrayOf("Easy", "Medium", "Hard")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(requireContext(), android.R.layout.simple_dropdown_item_1line, LEVEL)
     }
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    )
+//    : View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_option, container, false)
+//    }
 
 }
